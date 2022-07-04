ea377e3a09afa797b5e3954d9277451551c26b4b
ProgramCods <franciscojose.sanchezg1@gmail.com>
2022-07-04 14:11:54 +0200

    Proyecto creado

---
+++ src/main/java/metaenlace/apps/AppCitasMedicas/AppCitasMedicasApplication.java
@@ -0,0 +1,13 @@
+package metaenlace.apps.AppCitasMedicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCitasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCitasMedicasApplication.class, args);
+	}
+
+}
